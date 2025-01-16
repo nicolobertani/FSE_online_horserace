2a2998c22dc81bce562784f6cf73f034fb291d8d
nicolobertani <29360000+nicolobertani@users.noreply.github.com>
2025-01-16 10:47:41 +0000

    simplified config.py

--- binary_choices/config.py
+++ binary_choices/config.py
@@ -8,36 +8,10 @@ from .backend.shared_info import *
 # ******************************************************************************************************************** #
 class Constants(BaseConstants):
 
-    # ---------------------------------------------------------------------------------------------------------------- #
-    # --- Task-specific Settings --- #
-    # ---------------------------------------------------------------------------------------------------------------- #
-
-    # number of steps in the staircase procedure (see graph in the docs)
-    # note that <num_choices = x> implies a choice list with <2^x> choices, i.e. <2^x> possible switching points
-    # thus, for instance, <num_choice = 5> yields 32 different classifications for preferences towards risks
     num_choices = 5
-
-    # lottery payoffs (in currency units set in settings.py)  and probability (in %) for the high lottery outcome
-    # <lottery_hi> and <lottery_lo> define the "high" and "low" outcomes of the lottery ("Option A")
-    # <probability> determines the likelihood that the lottery pays the "high" outcome as percentage number
-    # thus, <probability = x> implies that the lottery pays <lottery_hi> in <x>% and <lottery_lo> in <100-x>%
-    lottery_hi = 300.00
-    lottery_lo = 0.00
-    probability = 50.00
-
-    # (initial) sure payoff, i.e. the certain payment in the first choice
-    # <sure_payoff> defines the certain amount offered as "Option B" in the first of <num_choices> choices
-    # the sure payoffs for subsequent choices are determined by <delta> (see below)
-    sure_payoff = 160.00
-
-    # (initial) increase/decrease in sure payoff
-    # while the first choice offers a fix payment of <sure_payoff>, "Option B" in subsequent choices depend on <delta>
-    # generally, for choice <i = 1, 2, ... num_choices>, <sure_payoff_i = sure_payoff + delta / 2 ^ (i-1)> if choice
-    # <i-1> = "A" and <sure_payoff_i = sure_payoff - delta / 2 ^ (i-1)> if choice <i-1> = "B"
-    # thus, if a subject chooses "A" ("B"), <sure_payoff_i> increases (decreases) by half of the previous rounds <delta>
-    # for example: if <sure_payoff = x> and <delta = y>, "Option B" offers <x +/- y/2> in choice 2, <x +/- y/2 +/- y/4>
-    # in choice 3, etc.
-    delta = 80.00
+    name_in_url = 'binary_choices'
+    players_per_group = None
+    num_rounds = max([31, len(shared_info['set_p_bisection']) * shared_info["number_bisection_steps"] + shared_info['number_test_questions'] + 1])
 
     # ---------------------------------------------------------------------------------------------------------------- #
     # --- Overall Settings and Appearance --- #
@@ -66,10 +40,3 @@ class Constants(BaseConstants):
     # if <results = False>, the template "Decision.html" will not be rendered
     results = True
 
-    # ---------------------------------------------------------------------------------------------------------------- #
-    # --- oTree Settings (Don't Modify) --- #
-    # ---------------------------------------------------------------------------------------------------------------- #
-
-    name_in_url = 'binary_choices'
-    players_per_group = None
-    num_rounds = len(shared_info['set_p_bisection']) * shared_info["number_bisection_steps"] + shared_info['number_test_questions'] + 1
