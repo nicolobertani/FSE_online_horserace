bd33f3c7681750b471ed6779aea3c04c1f130162
nicolobertani <29360000+nicolobertani@users.noreply.github.com>
2025-04-10 18:49:13 +0100

    uncommented lines in heroku_deployment

--- heroku_deployment.sh
+++ heroku_deployment.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# bash otree_setup.sh
-
-# heroku apps:create fse-online
-# heroku addons:create heroku-postgresql:essential-0 --app fse-online
-# heroku config:set OTREE_PRODUCTION=1
-# heroku config:set OTREE_AUTH_LEVEL='STUDY' 
-# heroku config:set OTREE_ADMIN_PASSWORD='I_am_the_admin'
+heroku apps:create fse-online
+heroku addons:create heroku-postgresql:essential-0 --app fse-online
+heroku config:set OTREE_PRODUCTION=1
+heroku config:set OTREE_AUTH_LEVEL='STUDY' 
+heroku config:set OTREE_ADMIN_PASSWORD='I_am_the_admin'
 
 git push heroku master
 sleep 20
